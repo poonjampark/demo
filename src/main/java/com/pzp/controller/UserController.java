8d9f06bebb5886411f30a1443a5a2878446cd62d
poonjampark <466545828@qq.com>
2018-04-14 23:13:02 +0800

    初步完成日志配置

--- src/main/java/com/pzp/controller/UserController.java
+++ src/main/java/com/pzp/controller/UserController.java
@@ -3,6 +3,8 @@
  */
 package com.pzp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +20,15 @@ import com.pzp.service.UserServiceImpl;
 @RestController
 @RequestMapping("user")
 public class UserController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserServiceImpl userServiceImpl;
 	
 	@RequestMapping("listUser")
 	public String listUser() {
-		System.out.println("进来了");
+		LOGGER.info("日志测试");
 		return userServiceImpl.listUser(1, 10);
 	}
 }
